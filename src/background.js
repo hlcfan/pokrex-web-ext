372124a800cdd0687ab97e78f717435c8f53c155
Alex S <hlcfan.yan@gmail.com>
2021-02-05 21:03:17 +0800

    Revert global listener change

--- src/background.js
+++ src/background.js
@@ -16,18 +16,3 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     })
   }
 });
-
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  function(details) {
-    for (var i = 0; i < details.requestHeaders.length; ++i) {
-      if (details.requestHeaders[i].name === "User-Agent") {
-        details.requestHeaders.splice(i, 1);
-        break;
-      }
-    }
-    return {requestHeaders: details.requestHeaders};
-  },
-  {urls: ["<all_urls>"]},
-  ["blocking", "requestHeaders"]
-);
-
