565cf744cdbf8d4f104e9568ddb8564b101c8ff9
Alex S <hlcfan.yan@gmail.com>
2021-04-27 22:34:56 +0800

    Revert "Upgrade to webpack 5"
    This reverts commit c2ecd6f6322231442a63ad0e6f9263518710a57b.

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ const config = {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loaders: 'vue-loader',
       },
       {
         test: /\.js$/,
@@ -43,7 +43,7 @@ const config = {
       },
       {
         test: /\.css$/,
-        use: ['vue-style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.scss$/,
@@ -85,28 +85,26 @@ const config = {
       paths: glob.sync(`${__dirname}/src/**/*`, { nodir: true }),
       whitelistPatterns: [/tagify/]
     }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'icons', to: 'icons', globOptions: {ignore: ['icon.xcf']} },
-        { from: 'prepare/prepare.html', to: 'prepare.html', transform: transformHtml },
-        { from: 'rooms/rooms.html', to: 'rooms.html', transform: transformHtml },
-        { from: 'sync/index.html', to: 'sync.html', transform: transformHtml },
-        {
-          from: 'manifest.json',
-          to: 'manifest.json',
-          transform: (content) => {
-            const jsonContent = JSON.parse(content);
-            jsonContent.version = version;
+    new CopyWebpackPlugin([
+      { from: 'icons', to: 'icons', ignore: ['icon.xcf'] },
+      { from: 'prepare/prepare.html', to: 'prepare.html', transform: transformHtml },
+      { from: 'rooms/rooms.html', to: 'rooms.html', transform: transformHtml },
+      { from: 'sync/index.html', to: 'sync.html', transform: transformHtml },
+      {
+        from: 'manifest.json',
+        to: 'manifest.json',
+        transform: (content) => {
+          const jsonContent = JSON.parse(content);
+          jsonContent.version = version;
 
-            if (config.mode === 'development') {
-              jsonContent['content_security_policy'] = "script-src 'self' 'unsafe-eval'; object-src 'self'";
-            }
+          if (config.mode === 'development') {
+            jsonContent['content_security_policy'] = "script-src 'self' 'unsafe-eval'; object-src 'self'";
+          }
 
-            return JSON.stringify(jsonContent, null, 2);
-          },
+          return JSON.stringify(jsonContent, null, 2);
         },
-      ]
-    }),
+      },
+    ]),
   ],
 };
 
